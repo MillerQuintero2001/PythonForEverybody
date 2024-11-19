59173f10a7556a6341d523147e2834a745361420
MillerQuintero2001 <miquinterog@unal.edu.co>
2024-11-19 06:43:47 -0500

    FInish Course #2
    Almost starting course #3

---
+++ Course #2/Assigment_10_2.py
@@ -0,0 +1,39 @@
+# Name: Miller Quintero
+# Date: Oct 31, 2024
+# Brief: Write a program to read through the mbox-short.txt and figure out the distribution by hour of 
+# the day for each of the messages. You can pull the hour out from the 'From ' line by finding the time
+# and then splitting the string a second time using a colon: From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+# Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.
+
+# While loop until do a succesful file read
+while True:
+    name = input("Enter file: ")
+    try:
+        fh = open(name)
+        break
+    except:
+        print("Error, file name specified doesn't exist in the directory")
+    
+ # Dictionary declaration   
+countDict = dict()
+
+# Iteration through the lines of the file
+for line in fh:
+    line = line.strip()
+    if line.startswith("From "):
+        words = line.split()
+        hour = words[5].split(':')
+        countDict[hour[0]] = countDict.get(hour[0],0) + 1
+        
+# # Now it's time to sort dictionary by key
+# orderedList = []
+# for k,v in countDict.items():
+#     orderedList.append((k,v))
+# orderedList.sort()
+
+# Finally, print it
+print(sorted(countDict))
+# for k,v in sorted(countDict):
+#     print(k,v)
+    
+        
